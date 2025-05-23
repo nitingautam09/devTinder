8f95d0371c9cfa52bcfda0d60e50918e50483a65
Nitin Gautam <nitin.gautam@globallogic.com>
2025-05-23 15:54:01 +0530

    impliment daniation and validation in schema and add timeStamp

--- src/config/dataBase.js
+++ src/config/dataBase.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose')
 const URL = 'mongodb+srv://glnitingautam:IcJjtxUM9eo5QuNP@cluster0.bktzyor.mongodb.net/devTinder'
 
 const connectDB = async ()=>{
-    const res=await mongoose.connect(URL)
+   await mongoose.connect(URL)
 }
 
 module.exports = connectDB;
